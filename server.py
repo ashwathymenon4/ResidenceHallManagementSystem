ae941db3e5450e9b27b52ac362eb2bec35fad50e
Ashwathy Mohan Menon <ashwathymenon497@gmail.com>
2021-11-17 14:09:12 -0500

    togglebar

--- server.py
+++ server.py
@@ -1099,7 +1099,7 @@ if __name__ == "__main__":
 
         HOST, PORT = host, port
         print("running on %s:%d" % (HOST, PORT))
-        app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
+        app.run(host=HOST, port=PORT, debug=False, threaded=threaded)
 
 
     run()
